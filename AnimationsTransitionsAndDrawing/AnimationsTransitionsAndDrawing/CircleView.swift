a4dfa9a85ca2cd1e37b254c5f61e5fc3f8cfa886
Felipe Lefèvre Marino <felipemarino91@gmail.com>
2017-12-08 00:15:18 -0200

    Designables of customizedView

---
+++ AnimationsTransitionsAndDrawing/AnimationsTransitionsAndDrawing/CircleView.swift
@@ -0,0 +1,64 @@
+//
+//  CircleView.swift
+//  AnimationsTransitionsAndDrawing
+//
+//  Created by Felipe Lefèvre Marino on 12/7/17.
+//  Copyright © 2017 Felipe Lefèvre Marino. All rights reserved.
+//
+
+import UIKit
+
+@objc protocol CustomizedView {
+    @objc optional var imageTintColor: UIColor? { get set }
+    var borderWidth: CGFloat { get set }
+    var borderColor: UIColor { get set }
+    var cornerRadius: CGFloat { get set }
+}
+
+@IBDesignable
+class CircleImageView: UIImageView, CustomizedView {
+    
+    @IBInspectable var imageTintColor: UIColor? {
+        didSet {
+            image = self.image?.withRenderingMode(.alwaysTemplate)
+            tintColor = imageTintColor
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable var borderColor: UIColor = UIColor.black {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.masksToBounds = true
+            layer.cornerRadius = cornerRadius
+        }
+    }
+}
+
+@IBDesignable
+class BorderedUIView: UIView, CustomizedView {
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable var borderColor: UIColor = UIColor.black {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.masksToBounds = true
+            layer.cornerRadius = cornerRadius
+        }
+    }
+}
